1a318d01c4a911f7461322ccef481fb8a92d8950
VladRodionov <vladrodionov@gmail.com>
2014-11-01 11:01:37 -0700

    DB-1933 patch

--- hbase-server/src/main/java/org/apache/hadoop/hbase/coprocessor/CoprocessorHost.java
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/coprocessor/CoprocessorHost.java
@@ -80,6 +80,8 @@ public abstract class CoprocessorHost<E extends CoprocessorEnvironment> {
   /** Ordered set of loaded coprocessors with lock */
   protected SortedSet<E> coprocessors =
       new SortedCopyOnWriteSet<E>(new EnvironmentPriorityComparator());
+  protected List<E> coprocessorList = new ArrayList<E>();
+  
   protected Configuration conf;
   // unique file prefix to use for local copies of jars when classloading
   protected String pathPrefix;
@@ -155,6 +157,13 @@ public abstract class CoprocessorHost<E extends CoprocessorEnvironment> {
     coprocessors.addAll(configured);
   }
 
+  protected void updateCoprocessorList()
+  {
+	  coprocessorList.clear();
+	  for( E e: coprocessors){
+		  coprocessorList.add(e);
+	  }
+  }
   /**
    * Load a coprocessor implementation into the host
    * @param path path to implementation jar
@@ -210,6 +219,7 @@ public abstract class CoprocessorHost<E extends CoprocessorEnvironment> {
       throws IOException {
     E env = loadInstance(implClass, priority, conf);
     coprocessors.add(env);
+    updateCoprocessorList();
   }
 
   /**
