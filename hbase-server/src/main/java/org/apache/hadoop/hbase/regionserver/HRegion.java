09e87ef27da8540d84da3fd21c30b87e761a1a1a
Jun Yuan <jyuan@splicemachine.com>
2014-11-25 15:43:14 -0800

    Revert "DB-2175 fix"
    This reverts commit 86e294d5c348f052cc3186d0667e6bfaa7f1222e.

--- hbase-server/src/main/java/org/apache/hadoop/hbase/regionserver/HRegion.java
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/regionserver/HRegion.java
@@ -5455,11 +5455,6 @@ public class HRegion implements HeapSize { // , Writable{
     return heapSize;
   }
 
-  public RegionSplitPolicy getRegionSplitPolicy()
-  {
-    return splitPolicy;
-  }
-  
   /*
    * This method calls System.exit.
    * @param message Message to print out.  May be null.
@@ -6163,6 +6158,4 @@ public class HRegion implements HeapSize { // , Writable{
   public Configuration getConf() {
     return conf;
   }
-  
-
 }
