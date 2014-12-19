1ab9daf7eb95858bfa232c547d6f714d2016efb7
VladRodionov <vladrodionov@gmail.com>
2014-12-19 13:11:36 -0800

    DB-2164: made InternalScan LimitedPrivate

--- hbase-server/src/main/java/org/apache/hadoop/hbase/regionserver/InternalScan.java
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/regionserver/InternalScan.java
@@ -18,12 +18,15 @@
  */
 package org.apache.hadoop.hbase.regionserver;
 
+import java.io.IOException;
+
+import org.apache.hadoop.hbase.HBaseInterfaceAudience;
 import org.apache.hadoop.classification.InterfaceAudience;
 import org.apache.hadoop.hbase.client.Get;
 import org.apache.hadoop.hbase.client.Scan;
 
 /**
- * Special internal-only scanner, currently used for increment operations to
+ * Special scanner, currently used for increment operations to
  * allow additional server-side arguments for Scan operations.
  * <p>
  * Rather than adding new options/parameters to the public Scan API, this new
@@ -33,8 +36,8 @@ import org.apache.hadoop.hbase.client.Scan;
  * {@link #checkOnlyMemStore()} or to only read from StoreFiles with
  * {@link #checkOnlyStoreFiles()}.
  */
-@InterfaceAudience.Private
-class InternalScan extends Scan {
+@InterfaceAudience.LimitedPrivate(HBaseInterfaceAudience.COPROC)
+public class InternalScan extends Scan {
   private boolean memOnly = false;
   private boolean filesOnly = false;
 
@@ -44,7 +47,12 @@ class InternalScan extends Scan {
   public InternalScan(Get get) {
     super(get);
   }
-
+  
+  public InternalScan(Scan scan) throws IOException
+  { 
+    super(scan); 
+  }
+  
   /**
    * StoreFiles will not be scanned. Only MemStore will be scanned.
    */
